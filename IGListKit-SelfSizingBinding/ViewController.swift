83b5c4b23bc18911a243af03c4057eebc69aa99d
harshilshah <harshilshah1910@outlook.com>
2017-05-13 10:22:46 +0530

    Initial Commit

---
+++ IGListKit-SelfSizingBinding/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IGListKit-SelfSizingBinding
+//
+//  Created by Harshil Shah on 13/05/17.
+//  Copyright © 2017 Harshil Shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
